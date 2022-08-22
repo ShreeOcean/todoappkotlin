8a79c71cd7c41898df7f8d52c52667f0541bc683
Pragyanshree Das <shree.pragyan23@gmail.com>
2022-08-22 18:50:24 +0530

    done with date and time picker code

--- app/src/main/java/com/ocean/todoinkotlin/view/fragment/AddTODOFragment.kt
+++ app/src/main/java/com/ocean/todoinkotlin/view/fragment/AddTODOFragment.kt
@@ -1,5 +1,6 @@
 package com.ocean.todoinkotlin.view.fragment
 
+import android.R
 import android.app.DatePickerDialog
 import android.app.TimePickerDialog
 import android.os.Bundle
@@ -7,9 +8,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.DatePicker
-import android.widget.TimePicker
+import android.widget.ArrayAdapter
 import com.ocean.todoinkotlin.databinding.FragAddTodoBinding
+import com.ocean.todoinkotlin.model.TodoModel
+import com.ocean.todoinkotlin.roomDB.AppDatabase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -25,6 +31,12 @@ class AddTODOFragment : Fragment() {
     var finalDate = 0L
     var finalTime = 0L
 
+    private val labels = arrayListOf("Personal", "Business", "Insurance", "Shopping", "Banking")
+
+    val db by lazy {
+        AppDatabase.getDatabase(requireContext())
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -32,14 +44,43 @@ class AddTODOFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = FragAddTodoBinding.inflate(inflater, container, false)
 
+        setUpSpinner()
+
         binding.etDate.setOnClickListener(View.OnClickListener {
             setDateListener()
         })
         binding.etTime.setOnClickListener(View.OnClickListener {
             setTimeListener()
         })
+        binding.saveBtn.setOnClickListener( View.OnClickListener {
+
+            val category = binding.spinnerCategory.selectedItem.toString()
+            val description = binding.outlinedTextFieldTittle.editText?.text.toString()
+            val title = binding.outlinedTextFieldTittle.editText?.text.toString()
+
+            GlobalScope.launch(Dispatchers.Main) {
+                val id = withContext(Dispatchers.IO) {
+                    return@withContext db.todoDao().insertToDo(
+                        TodoModel(
+                            title,
+                            description,
+                            category,
+                            finalDate,
+                            finalTime
+                        )
+                    )
+                }
+//                finish()
+            }
+        })
 
         return binding.root
+    }
+
+    private fun setUpSpinner() {
+        val adapter = ArrayAdapter<String>(requireContext(), R.layout.simple_spinner_dropdown_item, labels)
+        labels.sort()
+        binding.spinnerCategory.adapter = adapter
     }
 
     private fun setTimeListener(){
